2122146a791ef71c47d1da9f982ad326aa33a02d
yangyuting <seeyuyu@163.com>
2019-06-26 23:30:43 +0800

    轮播图数据

--- src/common/js/jsonp.js
+++ src/common/js/jsonp.js
@@ -1,7 +1,7 @@
 import originJSONP from 'jsonp'
 
-export default function jsonp(url, data, option) {
-  url +=(url.indexOf('?') < 0 ? '?' : '&') + param(data)
+export default function jsonp (url, data, option) {
+  url += (url.indexOf('?') < 0 ? '?' : '&') + param(data)
   return new Promise((resolve, reject) => {
     originJSONP(url, option, (err, data) => {
       if (!err) {
@@ -13,7 +13,7 @@ export default function jsonp(url, data, option) {
   })
 }
 
-function param(data) {
+function param (data) {
   let url = ''
   for (var i in data) {
     let value = data[i] !== undefined ? data[i] : ''
