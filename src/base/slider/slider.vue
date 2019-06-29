0fc6d951439ccc38b2a6a1ef5d5df872a05acd69
yangyuting <seeyuyu@163.com>
2019-06-29 23:32:10 +0800

    封装jsonp

--- src/base/slider/slider.vue
+++ src/base/slider/slider.vue
@@ -68,8 +68,7 @@ export default {
 </script>
 
 <style scoped lang="stylus" rel="stylesheet/stylus">
-@import '~common/stylus/variable';
-
+@import '~common/stylus/variable'
 .slider {
   min-height: 1px;
 
