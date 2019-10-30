49f62f140781a151356f672f5cd5eeda8dcbe2c8
yangyuting <seeyuyu@163.com>
2019-10-30 22:31:12 +0800

    歌手

--- src/base/listview/listview.vue
+++ src/base/listview/listview.vue
@@ -1,3 +1,53 @@
 <template>
-
+  <scroll @scroll="scroll"
+          :listen-scroll="listenScroll"
+          :probe-type="probeType"
+          :data="data"
+          class="listview"
+          ref="listview">
+    <ul>
+      <li v-for="(group,index) in data" :key="index" class="list-group" ref="listGroup">
+        <h2 class="list-group-title">{{group.title}}</h2>
+        <ul>
+          <li @click="selectItem(item)" v-for="(item,index) in group.items" :key="index" class="list-group-item">
+            <img class="avatar" v-lazy="item.avatar">
+            <span class="name">{{item.name}}</span>
+          </li>
+        </ul>
+      </li>
+    </ul>
+    <div class="list-shortcut" @touchstart.stop.prevent="onShortcutTouchStart"
+         @touchmove.stop.prevent="onShortcutTouchMove"
+         @touchend.stop>
+      <ul>
+        <li v-for="(item, index) in shortcutList" :data-index="index" class="item" :key="index"
+            :class="{'current':currentIndex===index}">{{item}}
+        </li>
+      </ul>
+    </div>
+    <div class="list-fixed" ref="fixed" v-show="fixedTitle">
+      <div class="fixed-title">{{fixedTitle}}</div>
+    </div>
+    <div v-show="!data.length" class="loading-container">
+      <loading></loading>
+    </div>
+  </scroll>
 </template>
+<script>
+import Scroll from 'base/scroll/scroll'
+// import Loading from 'base/loading/loading'
+
+export default {
+  props: {
+    data: {
+      type: Array,
+      default () {
+        return []
+      }
+    }
+  },
+  components: {
+    Scroll
+  }
+}
+</script>
